808f90e483e6b055ffaa7c26ca7b2e00c5184c95
njerimuriithi <cnjeri388@gmail.com>
2021-03-30 22:03:13 +0300

    second commit

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,43 +1,79 @@
 <template>
-<div class="images">
-   <!-- Gallery -->
+
 <div class="row">
-  <div class="col-lg-5 col-md-12 mb-2 mb-lg-0">
-  <img
-      src="../assets/homeimages/himage7.jpg"
-      class="w-100 shadow-1-strong rounded mb-4"
-      alt="bbjjnkn"
-    
-    />
+  <!--firstimage-->
+  <div class="col-lg-5 col-md-4 col-sm-2">
+     <div class="card">
+       <img class="card-img" src="../assets/homeimages/himage7.jpg" alt="firstimage">
+       <div class="card-img-overlay text-blue align-text-bottom ">
+         <h1 class="d-inline-flex ">01</h1>
+         <p class="fs-3 fw-bold">How to make a perfect green smoothie</p>
+       </div>
+  </div>
+  </div>
+  <!--secondimage-->
+   <div class="col-3">
+     <div class="card">
+       <img class="card-img-top" src="../assets/homeimages/redsmoothie.jpg" alt="firstimage">
+       <div class="card-body text-black">
+         <h1 class="d-inline-flex ">02</h1>
+         <p class="card-text fs-3 fw-bold">How to make a perfect RaspBerry smoothie</p>
+       </div>
+  </div>
+  </div>
+
+  <!--thirdimage-->
+   <div class="col-4">
+     <div class="card">
+       <img class="card-img-top" src="../assets/Salads/Greek-Salad.jpg" alt="firstimage">
+       <div class="card-body text-black">
+         <h1 class="d-inline-flex ">03</h1>
+         <p class="card-text fs-3 fw-bold">Greek Salad Recipe with Lemon Dressing</p>
+       </div>
+  </div>
+  </div>
+  </div>
+
+  <div class="row secondimg mt-4 ">
+
+   <div class="col-lg-4">
+     <div class="card">
+       <img class="card-img-top" src="../assets/homeimages/bananasmothie.jpg" alt="firstimage">
+       <div class="card-body text-dark">
+           <h1 class="d-inline-flex ">04</h1>
+         <p class="card-text fs-3 fw-bold">Banana Smoothies</p>
+       </div>
+  </div>
+  </div>
+
+
+    <div class="col-lg-3">
+     <div class="card">
+       <img class="card-img-top" src="../assets/Salads/fruitsalads.jpg" alt="firstimage">
+       <div class="card-body text-dark">
+           <h1 class="d-inline-flex ">05</h1>
+           <p class="card-text fs-3 fw-bold">Fruit Smoothies</p>
+       </div>
+  </div>
+  </div>
+
+    <div class="col-lg-5">
+     <div class="card">
+       <img class="card-img-top" src="../assets/Salads/weightlosssalads.jpg" alt="firstimage">
+       <div class="card-body text-dark">
+           <h1 class="d-inline-flex  ">06</h1>
+            <p class="card-text fs-3 fw-bold">weight loss salads</p>
+       </div>
+  </div>
+  </div>
+
 
   
 
-    <img
-      src="h../assets/homeimages/himage2.jpg"
-      class="w-100 shadow-1-strong rounded mb-4"
-      alt=""
-    />
-  </div>
 
-  <div class="col-lg-6 mb-4 mb-lg-0">
-    <img
-      src="../assets/homeimages/himage4.jpg"
-      class="w-100 shadow-1-strong rounded mb-4"
-      alt=""
-    />
+</div>
 
-    <img
-      src="../assets/homeimages/himage5.jpg"
-      class="w-100 shadow-1-strong rounded mb-4"
-      alt=""
-    />
-  </div>
- 
-</div>
- 
-  
- 
-</div>
+
 
   
 </template>
@@ -49,7 +85,12 @@ export default {
 </script>
 
 <style>
-
+.card-img-overlay{
+  margin-top: 500px;
+}
+h1{
+  background-color: grey;
+}
 
 
 </style>
